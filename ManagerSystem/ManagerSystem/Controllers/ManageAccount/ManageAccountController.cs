154b5ff7e82bb0f8421983217daa3e8a98695c24
v-jazhu <v-jazhu@microsoft.com>
2014-03-28 03:40:33 -0700

    328

---
+++ ManagerSystem/ManagerSystem/Controllers/ManageAccount/ManageAccountController.cs
@@ -0,0 +1,108 @@
+﻿using ManagerSystem.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerSystem.Controllers.ManageAccount
+{
+    public class ManageAccountController : Controller
+    {
+        public DBManagerReposity dbManager = new DBManagerReposity();
+        //
+        // GET: /ManageAccount/
+
+        public ActionResult Managers()
+        {
+            IQueryable<Manager> allManagers = dbManager.GetAllManagers();
+            return View(allManagers);
+        }
+
+        //
+        // GET: /ManageAccount/Details/5
+
+        public ActionResult ManagerDetails(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ManageAccount/Create
+
+        public ActionResult AddManager()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ManageAccount/Create
+
+        [HttpPost]
+        public ActionResult AddManager(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ManageAccount/Edit/5
+
+        public ActionResult EditManager(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ManageAccount/Edit/5
+
+        [HttpPost]
+        public ActionResult EditManager(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ManageAccount/Delete/5
+
+        public ActionResult DeleteManager(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ManageAccount/Delete/5
+
+        [HttpPost]
+        public ActionResult DeleteManager(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ManagerSystem/ManagerSystem/Models/DBModels/DBManagerReposity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerSystem.Models.DBModels
+{
+    public partial class DBManagerReposity
+    {
+        private ManagerSystemDataContext managerSystemDataContext = new ManagerSystemDataContext();
+
+        #region Login Operations
+        public bool Login(LoginModel loginModel)
+        {
+            IQueryable<Manager> managerQuery = managerSystemDataContext.Managers.Where(manager => manager.Name.Equals(loginModel.UserName) && manager.Password.Equals(loginModel.Password));
+            if (managerQuery.Count() == 1)
+            {
+                return true;
+            }
+            else
+            { return false; }
+        }
+        #endregion
+
+        #region Manage Account
+        public Manager GetManager(int managerId)
+        {
+            Manager managered = managerSystemDataContext.Managers.Where(manager => manager.ManagerId == managerId).SingleOrDefault();
+            if (managered != null)
+            {
+                return managered;
+            }
+            throw new Exception(string.Format("The Manager with ID: {0} doesn't exist or has been deleted.", managered));
+        }
+
+        public IQueryable<Manager> GetAllManagers()
+        {
+            return from manager in managerSystemDataContext.Managers
+                   select manager;
+        }
+
+        public void AddNewManager(Manager manager)
+        {
+            managerSystemDataContext.Managers.InsertOnSubmit(manager);
+        }
+
+        public void DeleteManager(Manager manager)
+        {
+            managerSystemDataContext.Managers.DeleteOnSubmit(manager);
+        }
+
+        public void Save()
+        {
+            managerSystemDataContext.SubmitChanges();
+        }
+        #endregion
+    }
+}
